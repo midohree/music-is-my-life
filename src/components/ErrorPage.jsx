2473de37ee67fb1067f047823830c51a38970ff1
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-02-06 02:51:46 +0900

    :recycle: seperate reusable components

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,5 @@
+function ErrorPage() {
+  return <h3>에러가 발생했습니다. 다시 시도해주세요.</h3>;
+}
+
+export default ErrorPage;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Loader from 'react-loader-spinner';
+
+import { theme } from './styles/theme';
+import { fadeIn } from './styles/keyframes';
+
+const Wrapper = styled.div`
+  width: inherit;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  text-align: center;
+`;
+
+const StyledLoader = styled(Loader)`
+  animation: 0.6s ${fadeIn};
+`;
+
+const Loading = ({ children }) => {
+  return (
+    <Wrapper>
+      {children || <StyledLoader type='Circles' color={theme.salmon} width={55} height={55}/>}
+    </Wrapper>
+  );
+};
+
+Loading.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Loading;
