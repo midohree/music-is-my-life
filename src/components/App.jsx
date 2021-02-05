88bbbd47ae1c23ebe147c6e9e5855f6e9aa7182a
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-02-06 02:49:40 +0900

    :lipstick: add default styles (font, theme, layout)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,9 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
+import styled, { ThemeProvider } from 'styled-components';
+
+import { theme } from './styles/theme';
+import { flexCenter } from './styles/layout';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 import Home from './Home';
 import TopAlbums from './TopAlbums';
@@ -6,21 +11,24 @@ import Album from './Album';
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/topAlbum'>
-          <TopAlbums />
-        </Route>
-        <Route exact path='/topAlbums/:album_id'>
-          <Album />
-        </Route>
-        <Redirect to='/' />
-      </Switch>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/topAlbums' component={TopAlbums} />
+          <Route exact path='/topAlbums/:album_id' component={Album} />
+          <Redirect to='/' />
+        </Switch>
+      </ThemeProvider>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  ${flexCenter}
+  width: 100vw;
+  height: 100vh;
+`;
 
 export default App;
